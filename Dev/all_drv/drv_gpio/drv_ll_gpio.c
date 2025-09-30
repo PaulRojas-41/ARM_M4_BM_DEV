ce2a52717cb51225e9334510e26b15443e626184
Paul Rojas <sirpaul.tapia@hotmail.com>
2025-09-30 17:15:00 +0200

    [ARM_M4_SysTick_Timer] Systick timer for delay API implementation
    - This example contains:
    	* SysTick timer peripheral configuration
    	* API for delay in ms
    	* Led Blinking periodically every second (main applicatif)
    	* info_links.txt: link reference for Cortex-M4 generic
    	  peripherals (Section 4.4 System timer)
    - Flashed into board: green led and blue red blinking every 1 second

--- Dev/all_drv/drv_gpio/drv_ll_gpio.c
+++ Dev/all_drv/drv_gpio/drv_ll_gpio.c
@@ -16,37 +16,68 @@
  ******************************************************************************
  */
  #include "stm32f4xx.h"
- 
-  /* Jump into main applicative SW */
+
+ /* SYSTICK_ENABLE = Enable the counter
+    SYSTICK Countflag = SW can use this bit state to determine if SysTick has ever counted to zero 
+    SYSTICK ClockSource = Selects system or external clock source  */
+
+ #define SYSTICK_CSR_ENABLE    (1 << 0)
+ #define SYSTICK_CSR_CLOCKSRC  (1 << 2)
+ #define SYSTICK_CSR_COUNTFLAG (1 << 16) 
+ #define SYSTICK_CYCLES_PER_MS (16000u) 
+
+ /* Local prototypes */
+ void SysTick_DelayMs(uint32_t delay_ms);
+ void SysTick_Init(void);
+
+/* Jump into main applicative SW */
 
  int main(void)
  {
     
-     /* - Enable  CLK Peripheral for GPIOD and GPIOA: AHB1ENR
+     /* GPIO peripheral drv config: 
+        - Enable  CLK Peripheral for GPIOD and GPIOA: AHB1ENR
         - Configure Port Mode: I/O, GPIOD here as output for led blink 
         - User Push button PA0 = GPIOA reset state is equal to be in Input mode */
       
      RCC->AHB1ENR |= (1 << 0) | (1 << 3);
-     GPIOD->MODER |= (1 << (14 * 2)) | (1 <<(13 * 2)) | (1 <<(12 * 2));
+     GPIOD->MODER |= (1 << (15 * 2)) | (1 <<(12 * 2));
     
+     /* SysTick drv config:
+        - Write Reload value: number of cycles for each ms delay 
+        - Clear current value / CountFlag CSR reg
+        - Set CSR register: When Enable = 1, counter takes Reload value */
+
+    SysTick_Init();
+
      /* Loop */
 
      while(1)
      {
-         for(int i=0;i<1000000;i++); /* Mock delay: instruction's cycles */
-         GPIOD->ODR ^= (1<<12); /* Write value: Toogle LED */
-         for(int i=0;i<1000000;i++);
-         GPIOD->ODR ^= (1<<13);
-
-         do
-         {
-             GPIOD->ODR |= (1<<14);
-         }while((GPIOA->IDR & 0x01) != 0u);
-         
-         if((GPIOA->IDR & 0x01) == 0u)
-         {
-             GPIOD->ODR &= ~(1 << 14);
-         }
+         SysTick_DelayMs(1000u);
+         GPIOD->ODR ^= (1<<12);
+         SysTick_DelayMs(1000u);
+         GPIOD->ODR ^= (1<<15);
      }
- }
+}
+
+/* Local method's implementation */
+
+void SysTick_Init(void)
+{
+    SysTick->LOAD = SYSTICK_CYCLES_PER_MS - 1; 
+    SysTick->VAL  = 0;
+    SysTick->CTRL |= SYSTICK_CSR_ENABLE | SYSTICK_CSR_CLOCKSRC;
+}
+
+void SysTick_DelayMs(uint32_t delay_ms)
+{
+    while(delay_ms)
+    {
+        if(SysTick->CTRL & SYSTICK_CSR_COUNTFLAG)
+        {
+            delay_ms--;
+        }
+    }
+}
  
